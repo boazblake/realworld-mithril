603790ac473876dab4f0bae3cd48528451682dc6
bblake <bblake@goempyrean.com>
2019-06-01 12:51:15 -0500

    tilde path

--- docs/src.a2b27638.js
+++ docs/src.a2b27638.js
@@ -6615,7 +6615,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56418" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58199" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
