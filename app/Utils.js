79f1971f1c822236bf1f305eab4f94955f0ae342
bblake <bblake@goempyrean.com>
2020-07-12 12:09:52 -0500

    making images more secure

--- app/Utils.js
+++ app/Utils.js
@@ -3,8 +3,11 @@ export const log = (m) => (v) => {
   return v
 }
 
+const secureImg = (url) =>
+  url.match(/(https)./) ? url : url.replace("http", "https")
+
 export const sanitizeImg = (url) =>
-  url && url.match(/\.(jpeg|jpg|gif|png|svg)$/) ? url : ""
+  url && url.match(/\.(jpeg|jpg|gif|png|svg)$/) ? secureImg(url) : ""
 
 export const errorViewModel = (err) =>
   Object.keys(err).map((k) => ({ key: k, values: err[k] }))
